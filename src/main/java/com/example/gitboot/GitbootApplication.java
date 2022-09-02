a44b28a7777ffb36a8d1257ea3629d0b0b95aca2
yb-dlxh <yb-dlxh@163.com>
2022-09-02 17:04:50 +0800

    gitboot commit 1 create project

---
+++ src/main/java/com/example/gitboot/GitbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitboot/GitbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
