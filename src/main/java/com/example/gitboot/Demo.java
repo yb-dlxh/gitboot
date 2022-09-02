78ef10df1ef859d887e6a8dce00fa56bd4919cec
yb-dlxh <yb-dlxh@163.com>
2022-09-02 17:37:59 +0800

    modify2 Demo.java

--- src/main/java/com/example/gitboot/Demo.java
+++ src/main/java/com/example/gitboot/Demo.java
@@ -3,6 +3,7 @@ package com.example.gitboot;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello my git");
 
     }
 }
